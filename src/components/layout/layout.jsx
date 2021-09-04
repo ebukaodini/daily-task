27c3f9c4b0dff9752d203651856a58c011f79746
Ebuka Odini <ebukaodini@gmail.com>
2021-09-04 14:59:59 +0100

    implemented theming in the application and linked it to the redux store ui

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,13 +1,21 @@
-
-import { BottomNavigation, Header, SideNavigation } from '..';
+import Header from "../header/header";
+import SideNavigation from "../navigation/side/side";
+import BottomNavigation from "../navigation/bottom/bottom";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../../store/ui/ui.slice";
 import './layout.scss';
 
 export default function Layout(props) {
+
+  const theme = useSelector(state => state.ui);
+  const dispatch = useDispatch();
+
   return (
-    <>
+    <div className={`${theme}`}>
 
       <header className='container'>
         <Header />
+        <button onClick={() => dispatch(toggleTheme())}>Toggle Theme</button>
       </header>
 
       <main className='container'>
@@ -25,6 +33,6 @@ export default function Layout(props) {
         <BottomNavigation />
       </nav>
 
-    </>
+    </div>
   )
 }
