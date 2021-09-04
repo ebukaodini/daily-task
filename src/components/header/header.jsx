32734f00bbf7c3afb585708bf08dc906b37bd4c9
Ebuka Odini <ebukaodini@gmail.com>
2021-09-04 23:03:15 +0100

    added the header component, updated the layout component, updated the base.scss module

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,37 @@
-export default function Header(props) {
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Settings } from "react-feather";
+import { formatDate } from '../../utils/helpers/formatDate';
+import './header.scss';
+
+export default function Header() {
+  const [date, setDate] = useState(formatDate());
+
+  useEffect(() => {
+    setInterval(() => {
+      setDate(`${formatDate()}`);
+    }, 30000);
+  }, [setDate]);
+
   return (
-    <div className={props.className}>
-      Header
+    <div className='header'>
+
+      <div className='header__title'>
+
+        <h1 className='__title'>Daily Task</h1>
+
+        <h4 className='__subtitle'>{date}</h4>
+
+      </div>
+
+      <div className='header__actions'>
+        
+        <Link to='/settings' className='__settings_btn'>
+          <Settings />
+        </Link>
+
+      </div>
+
     </div>
   )
 }
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,21 +1,18 @@
 import Header from "../header/header";
 import SideNavigation from "../navigation/side/side";
 import BottomNavigation from "../navigation/bottom/bottom";
-import { useSelector, useDispatch } from "react-redux";
-import { toggleTheme } from "../../store/ui/ui.slice";
+import { useSelector } from "react-redux";
 import './layout.scss';
 
 export default function Layout(props) {
 
   const theme = useSelector(state => state.ui);
-  const dispatch = useDispatch();
-
+  
   return (
     <div className={`${theme}`}>
 
       <header className='container'>
         <Header />
-        <button onClick={() => dispatch(toggleTheme())}>Toggle Theme</button>
       </header>
 
       <main className='container'>
