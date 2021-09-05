821fe280452690e62bfa2d98952cd9d9ddd93c88
Ebuka Odini <ebukaodini@gmail.com>
2021-09-05 15:22:59 +0100

    added the page title component

---
+++ src/components/title/title.jsx
@@ -0,0 +1,28 @@
+import { ArrowLeft } from "react-feather";
+import { useHistory } from "react-router";
+import './title.scss';
+
+/**
+ * component to render page title
+ * @param {String} title the page title
+ * @param {bool} shouldPop enable back button
+ * @returns 
+ */
+export default function Title({ title, shouldPop }) {
+
+  // default parameters
+  shouldPop = shouldPop || false;
+
+  const history = useHistory();
+
+  return (
+    <div className='__page_title'>
+      {shouldPop === true &&
+        <button className='__back_button' onClick={() => history.goBack()}>
+          <ArrowLeft />
+        </button>
+      }
+      <h2 className='__title'>{title}</h2>
+    </div>
+  )
+}
