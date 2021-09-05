ef13083aae487a2dc1d1ed9a5c2e34dc40d8024b
Ebuka Odini <ebukaodini@gmail.com>
2021-09-05 02:13:22 +0100

    fixed bug on the formatDate function

--- src/utils/helpers/formatDate.js
+++ src/utils/helpers/formatDate.js
@@ -4,11 +4,10 @@ export const formatDate = () => {
   let day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()];
 
   let hour = date.getHours();
-  let meridian = hour > 12 ? 'AM' : hour === 0 ? 'AM' : 'PM';
+  let meridian = hour >= 12 ? 'PM' : 'AM';
   hour = hour > 12 ? hour - 12 : hour === 0 ? 12 : hour;
 
   let minute = date.getMinutes().toString().padStart(2, '0');
-
 
   return `${day}, ${hour}:${minute} ${meridian}`;
 }
