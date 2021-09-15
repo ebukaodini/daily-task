c1c0d72682c4edfb68587411dfac42e76ea81a34
Ebuka Odini <ebukaodini@gmail.com>
2021-09-15 17:01:26 +0100

    updated the root app to initiate the app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,37 @@ import BacklogPage from './pages/BacklogPage/BacklogPage';
 import SettingsPage from './pages/SettingsPage/SettingsPage';
 import TasksPage from './pages/TasksPage/TasksPage';
 import ArchivePage from './pages/ArchivePage/ArchivePage';
+import { useEffect } from 'react';
+import { getSettings } from './store/settings/settings.actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTasks } from './store/tasks/tasks.actions';
+import { Themes } from './utils/constants/themes';
+import { setTheme } from './store/ui/ui.slice';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const settings = useSelector(state => state.settings);
+
+  // get initial data from database
+  useEffect(() => {
+    function __init() {
+      dispatch(getSettings())
+      dispatch(getTasks());
+    }
+    
+    // wait one second to allow db load
+    // Warning!!! Do not remove this event from the hook.
+    setTimeout(() => {
+      __init();
+    }, 1000);
+
+  }, [dispatch])
+  
+  // set theme
+  useEffect(() => {
+    dispatch(setTheme(settings.darkMode === true ? Themes.dark : Themes.light));
+  }, [dispatch, settings.darkMode])
 
   return (
     <Router>
