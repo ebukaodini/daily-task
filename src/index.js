237e8b82f7e7856eba8a2ab9637a772cca6ffa67
Ebuka Odini <ebukaodini@gmail.com>
2021-09-23 15:07:21 +0100

    update

--- src/index.js
+++ src/index.js
@@ -27,34 +27,34 @@ serviceWorkerRegistration.register();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// A2HS
-let deferredPrompt;
-// const addBtn = document.querySelector('.add-button');
-const addBtn = document.querySelector('.__A2HS');
-// addBtn.style.display = 'none';
+// // A2HS
+// let deferredPrompt;
+// // const addBtn = document.querySelector('.add-button');
+// const addBtn = document.querySelector('.__A2HS');
+// // addBtn.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI to notify the user they can add to home screen
+//   addBtn.style.display = 'block';
 
-  addBtn.addEventListener('click', (e) => {
-    alert('hi')
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
-});
+//   addBtn.addEventListener('click', (e) => {
+//     alert('hi')
+//     // hide our user interface that shows our A2HS button
+//     addBtn.style.display = 'none';
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//         if (choiceResult.outcome === 'accepted') {
+//           console.log('User accepted the A2HS prompt');
+//         } else {
+//           console.log('User dismissed the A2HS prompt');
+//         }
+//         deferredPrompt = null;
+//       });
+//   });
+// });
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useLayoutEffect } from 'react';
 import { Download } from 'react-feather';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -5,6 +6,43 @@ import './WelcomePage.scss';
 
 export default function WelcomePage() {
   const ui = useSelector(state => state.ui);
+
+
+
+  useLayoutEffect(() => {
+
+    // A2HS
+    let deferredPrompt;
+    // const addBtn = document.querySelector('.add-button');
+    const addBtn = document.querySelector('.__A2HS');
+    // addBtn.style.display = 'none';
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      // addBtn.style.display = 'block';
+
+      addBtn.addEventListener('click', (e) => {
+        alert('hi')
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+      });
+    });
+  })
 
   return (
     <section className={`${ui.theme}`}>
