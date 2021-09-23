902027fdee7225494a3d58b01a060a5727381253
Ebuka Odini <ebukaodini@gmail.com>
2021-09-23 20:35:47 +0100

    added documentation tasks

--- src/utils/constants/welcome-tasks.js
+++ src/utils/constants/welcome-tasks.js
@@ -37,6 +37,38 @@ export const WelcomeTasks =
   },
   {
     id: '2021-09-23T18:51:20.157Z',
+    description: 'Description. You can edit task description after adding them. Double click on the description to edit.',
+    timestamp: new Date().toUTCString(),
+    subtasks: [],
+    board: Board.tasks,
+    status: TaskStatus.pending,
+    tags: [],
+    order: 0,
+    history: [
+      {
+        timestamp: new Date().toUTCString(),
+        event: TaskStatus.pending
+      }
+    ]
+  },
+  {
+    id: '2021-09-23T18:51:21.157Z',
+    description: 'Tags. Click the more button to add and remove tags for your task.',
+    timestamp: new Date().toUTCString(),
+    subtasks: [],
+    board: Board.tasks,
+    status: TaskStatus.pending,
+    tags: [],
+    order: 0,
+    history: [
+      {
+        timestamp: new Date().toUTCString(),
+        event: TaskStatus.pending
+      }
+    ]
+  },
+  {
+    id: '2021-09-23T18:51:22.157Z',
     description: 'And yeah, you can add subtasks and update the status of your task as you go on.',
     timestamp: new Date().toUTCString(),
     subtasks: [
@@ -61,7 +93,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:21.157Z',
+    id: '2021-09-23T18:51:23.157Z',
     description: 'Settings. You can modify the title of the app in the settings.',
     timestamp: new Date().toUTCString(),
     subtasks: [],
@@ -77,7 +109,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:22.157Z',
+    id: '2021-09-23T18:51:24.157Z',
     description: 'Settings. You can enable dark mode.',
     timestamp: new Date().toUTCString(),
     subtasks: [],
@@ -93,7 +125,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:23.157Z',
+    id: '2021-09-23T18:51:25.157Z',
     description: 'Settings. Automation actions automatically reset the tasks board at the end of each day. Check subtasks below for more.',
     timestamp: new Date().toUTCString(),
     subtasks: [
@@ -130,7 +162,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:24.157Z',
+    id: '2021-09-23T18:51:26.157Z',
     description: 'Thanks for seeing this app. You can install it on your device as it works offline. Thanks to PWA.',
     timestamp: new Date().toUTCString(),
     subtasks: [],
@@ -146,7 +178,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:25.157Z',
+    id: '2021-09-23T18:51:27.157Z',
     description: 'Feel free to contribute to the repository. And please star the repository if you like the app. Enjoy the app 🙂❤️.',
     timestamp: new Date().toUTCString(),
     subtasks: [],
@@ -162,7 +194,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:26.157Z',
+    id: '2021-09-23T18:51:28.157Z',
     description: 'Now you can set all these tasks to Done. 😉',
     timestamp: new Date().toUTCString(),
     subtasks: [],
@@ -178,7 +210,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:27.157Z',
+    id: '2021-09-23T18:51:29.157Z',
     description: 'This task would be done tommorrow.',
     timestamp: new Date().toUTCString(),
     subtasks: [],
@@ -194,7 +226,7 @@ export const WelcomeTasks =
     ]
   },
   {
-    id: '2021-09-23T18:51:28.157Z',
+    id: '2021-09-23T18:51:30.157Z',
     description: 'This task has been completed.',
     timestamp: new Date().toUTCString(),
     subtasks: [],
