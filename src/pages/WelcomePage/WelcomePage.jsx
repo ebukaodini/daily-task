870de97f7c0d086772d4dab1f3e1957afaff13c3
Ebuka Odini <ebukaodini@gmail.com>
2021-09-23 16:20:26 +0100

    update

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Download } from 'react-feather';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -7,42 +7,41 @@ import './WelcomePage.scss';
 export default function WelcomePage() {
   const ui = useSelector(state => state.ui);
 
+  const [hasEvent, setHasEvent] = useState(false);
+  let deferredPrompt;
+
   useEffect(() => {
     // A2HS
-    const addBtn = document.querySelector('.__A2HS');
-    addBtn.style.display = 'none';
-
-    const a2HS = (e) => {
+    const setA2HSEvent = (e) => {
       console.log("Before Install Prompt", e);
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
-
-      // Update UI to notify the user they can add to home screen
-      addBtn.style.display = 'block';
-
-      addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        e.prompt();
-        // Wait for the user to respond to the prompt
-        e.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          e = null;
-        });
-      });
+      if (!hasEvent) {
+        deferredPrompt = e;
+        setHasEvent(true);
+      }
     }
 
-    window.addEventListener('beforeinstallprompt', a2HS);
-
+    window.addEventListener('beforeinstallprompt', setA2HSEvent);
     return () => {
-      window.removeEventListener('beforeinstallprompt', a2HS);
+      window.removeEventListener('beforeinstallprompt', setA2HSEvent);
     }
   }, []);
+
+  const a2HS = (e) => {
+    e.preventDefault();
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  }
 
   return (
     <section className={`${ui.theme}`}>
@@ -53,7 +52,9 @@ export default function WelcomePage() {
         </div>
 
         <div>
-          <button className='__A2HS'>
+          <button
+            onClick={a2HS}
+            className='__A2HS'>
             <Download size={18} />
             <span>Add to Home Screen</span>
           </button>
